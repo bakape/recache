907cfa0abd1b66153b8ffb89af4187aa7355afd5
bakape <scorpid33@gmail.com>
2019-05-07 12:03:33 +0300

    Multiple cache concurrency test

--- frontend_test.go
+++ frontend_test.go
@@ -128,4 +128,57 @@ func TestGetRecordConcurentFrontends(t *testing.T) {
 	wg.Wait()
 }
 
-// TODO: Test with concurrent caches
+func TestGetRecordConcurentCaches(t *testing.T) {
+	t.Parallel()
+
+	var wg sync.WaitGroup
+	wg.Add(9 * 3)
+
+	for j := 0; j < 3; j++ {
+		go func() {
+			var cache = NewCache(0, 0)
+
+			for i := 0; i < 3; i++ {
+				go func() {
+					f := cache.NewFrontend(dummyGetter)
+					for j := 0; j < 3; j++ {
+						go func(j int) {
+							key := "key" + strconv.Itoa(j)
+
+							defer wg.Done()
+							var keyWg sync.WaitGroup
+							keyWg.Add(6)
+
+							run := func() {
+								for k := 0; k < 3; k++ {
+									go func(k int) {
+										defer keyWg.Done()
+
+										var w bytes.Buffer
+										_, err := f.WriteTo(key, &w)
+										if err != nil {
+											t.Fatal(err)
+										}
+										assertEquals(t, unzip(t, &w),
+											fmt.Sprintf(`"%s"`, key))
+									}(k)
+								}
+							}
+
+							// Initial population and 2 caches reads concurrently
+							run()
+
+							// 3 reads after the data has been populated and made
+							// immutable
+							run()
+
+							keyWg.Wait()
+						}(j)
+					}
+				}()
+			}
+		}()
+	}
+
+	wg.Wait()
+}
