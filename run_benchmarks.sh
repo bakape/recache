5bf8de2ec00c4a3e01510471faab923c8783561a
bakape <bakape@gmail.com>
2020-10-04 07:52:43 +0300

    benchmarks: move print_values module

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -5,4 +5,4 @@ rm -f .bench_log
 for	i in {1..10}; do
 	go test --bench . --benchtime 200x ./benchmarks/ >> .bench_log
 done
-go run ./print_values/main.go > benchmark_results.csv
+go run ./benchmarks/print_values/main.go > benchmark_results.csv
