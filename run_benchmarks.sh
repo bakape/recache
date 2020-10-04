2c2111638a7bab72291e13221d455b6cdcf3dc44
bakape <bakape@gmail.com>
2020-10-04 07:56:19 +0300

    benchmarks: print summary after run

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -5,4 +5,5 @@ rm -f .bench_log
 for	i in {1..10}; do
 	go test --bench . --benchtime 200x ./benchmarks/ >> .bench_log
 done
+benchstat ./.bench_log
 go run ./benchmarks/print_values/main.go > benchmark_results.csv
